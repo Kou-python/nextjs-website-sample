b7ab6febd040c0145d16c2a59310518e8d58f1e9
Kou-python <25930127+Kou-python@users.noreply.github.com>
2024-11-13 16:07:01 +0900

    ハンバーガーメニュー（4章）完了[useState]

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -2,14 +2,17 @@
 import styles from "./index.module.css"
 import Link from "next/link"
 import Image from "next/image"
+import cx from "classnames"
+import { useState } from "react";
 
 export default function Menu() {
-  const open = () => {
-    document.querySelector("nav")?.classList.add(styles.open)
-  }
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const open = () => setOpen(true)
+  const close = () => setOpen(false)
+
   return (
 		<div>
-			<nav className={styles.nav}>
+			<nav className={cx(styles.nav, isOpen&&styles.open)}>
 				<ul>
 					<li className={styles.items}>
 						<Link href="news">ニュース</Link>
@@ -20,7 +23,16 @@ export default function Menu() {
 					<li className={styles.items}>
 						<Link href="contact">お問い合わせ</Link>
 					</li>
-				</ul>
+        </ul>
+        <button className={cx(styles.button, styles.close)} onClick={close}>
+          <Image
+            src="/close.svg"
+            alt="close"
+            width={24}
+            height={24}
+            priority
+          />
+        </button>
 			</nav>
 			<button className={styles.button} onClick={open}>
 				<Image src="/menu.svg" alt="menu" width={24} height={24} />
